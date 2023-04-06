abae1359173152b0f37a907d3c10059feed3d1c1
Sira <siraphop_2538@hotmail.com>
2023-04-06 18:44:56 +1000

    Update html/css and implement add book function

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-const hi = 'hi';
-console.log(hi);
+const addButton = document.querySelector('.add');
+const form = document.querySelector('.form');
+const overlay = document.querySelector('.overlay');
+
+class Book {
+  constructor() {
+    this.displayForm();
+    this.overlayRemove();
+  }
+
+  displayForm() {
+    addButton.addEventListener('click', () => {
+      form.classList.remove('hide');
+      overlay.classList.remove('hide');
+    });
+  }
+
+  overlayRemove() {
+    overlay.addEventListener('click', e => {
+      form.classList.add('hide');
+      overlay.classList.add('hide');
+    });
+  }
+}
+
+const exampleBook = new Book();
